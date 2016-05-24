6d094d1c1bb4c0ec9fb6fd85e7db0dd7b832a68f
Chris J Clarke <mrmr.mail@gmail.com>
2016-05-24 09:57:16 +0100

    video loads from spreadsheet

--- embeds/leicester-city-video-1/_source/main.js
+++ embeds/leicester-city-video-1/_source/main.js
@@ -1,14 +1,57 @@
 // console.log("loaded");
 
-// function lcfc_Video() { 
+// function lcfc_Video() {
 //     document.getElementById("mp4_src").src = "@@assetPath@@/leicestercity-celebration-cut.mp4";
 //     document.getElementById("leicesters-journey__videoLoader").load();
 // }
 
+// var lcfcVideoSrc = document.getElementById("leicester__video--loader");
+//
+// function isScrolledIntoView(element) {
+//     var elementTop    = element.getBoundingClientRect().top,
+//         elementBottom = element.getBoundingClientRect().bottom;
+//
+//     return elementTop < window.innerHeight && elementBottom >= 0;
+// }
+//
+// var lcfcVideo = function(){
+// 	if (isScrolledIntoView(document.getElementById("leicester__video--loader"))) {
+// 		lcfcVideoSrc.play(0);
+// 	} else {
+// 		lcfcVideoSrc.pause(0);
+// 	}
+// }
+//
+// window.onscroll = function() {
+//     lcfcVideo();
+// };
+
+
+
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+
 var lcfcVideoSrc = document.getElementById("leicester__video--loader");
 
+
 function isScrolledIntoView(element) {
-    var elementTop    = element.getBoundingClientRect().top,
+      var elementTop = element.getBoundingClientRect().top,
         elementBottom = element.getBoundingClientRect().bottom;
 
     return elementTop < window.innerHeight && elementBottom >= 0;
@@ -16,11 +59,14 @@ function isScrolledIntoView(element) {
 
 var lcfcVideo = function(){
 	if (isScrolledIntoView(document.getElementById("leicester__video--loader"))) {
-		lcfcVideoSrc.play(0);
+      loadJSON("http://interactive.guim.co.uk/docsdata-test/1g-nqRQPtxr70cnfyttZSrPJiSkiSgVNam9J53drsZ2g.json", function(data) {
+        lcfcVideoSrc.innerHTML = data.sheets.Sheet1[1].Content;
+        lcfcVideoSrc.play(0);
+      })
 	} else {
-		lcfcVideoSrc.pause(0);
-	}
-}
+		    lcfcVideoSrc.pause(0);
+	     }
+  }
 
 window.onscroll = function() {
     lcfcVideo();
